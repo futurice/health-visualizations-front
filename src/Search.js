8ea0e451bbf2f360147283152490ad035afb6c79
Chang Rajani <chang.rajani@gmail.com>
2017-07-28 16:58:58 +0300

    Work on search bar to search view

---
+++ src/Search.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Search extends Component {
+  render() {
+    return (
+      <div className="search-page">
+          <h1> Nettipuoskari search </h1>
+      </div>
+    );
+  }
+}
--- src/components/QueryForm.js
+++ src/components/QueryForm.js
@@ -2,12 +2,20 @@ import React from 'react';
 import { render } from 'react-dom';
 import '../css/QueryForm.css';
 
+import {
+  Link
+} from 'react-router-dom'
+
+import { Redirect } from 'react-router';
+
+
 export default class QueryForm extends React.Component {
 
    constructor(props) {
     super(props);
     this.state = {
-      inputValue: ''
+      inputValue: '',
+      redirect: false
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -24,10 +32,17 @@ export default class QueryForm extends React.Component {
   onSubmit(event) {
     event.preventDefault() // Stop full page reload
     this.props.onSubmit(this.state.inputValue);
+    
+    this.setState({
+      redirect: true
+    })
   }
 
   render() {
-    return (
+    if (this.state.redirect) {
+      return <Redirect push to="/sample" />;
+    }
+    return ( 
       <div className="form">
         <form onSubmit={this.onSubmit}>
           <input className="input" onChange={this.handleChange} placeholder="Search e.g burana" />
