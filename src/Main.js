f666bd56f3b6e77cc8bcc268e944929ef1d3db1d
Chang Rajani <chang.rajani@gmail.com>
2017-08-28 10:39:47 +0300

    Add react-ga

--- src/Main.js
+++ src/Main.js
@@ -8,10 +8,18 @@ import Landing from './components/Landing';
 import Search from './components/Search';
 import NotFound from './components/NotFound';
 
+var ReactGA = require('react-ga');
+ReactGA.initialize('UA-105305355-1');
+
+function logPageView() {
+  ReactGA.set({ page: window.location.pathname + window.location.search });
+  ReactGA.pageview(window.location.pathname + window.location.search);
+}
+
 export default class Main extends Component {
   render() {
     return (
-      <Switch>
+      <Switch onUpdate={logPageView}>
         <Route exact path="/" component={ Landing } />
         <Route path="/search/:keyword" component={ Search } />
         <Route path='/not_found/:keyword' component={ NotFound } />
