5900543aaab43ce60883cd6a10d81d8649c505f6
Chang Rajani <chang.rajani@gmail.com>
2017-08-28 10:43:41 +0300

    Fix google analytics

--- src/index.js
+++ src/index.js
@@ -4,8 +4,16 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
 
+var ReactGA = require('react-ga');
+ReactGA.initialize('UA-105305355-1');
+
+function logPageView() {
+  ReactGA.set({ page: window.location.pathname + window.location.search });
+  ReactGA.pageview(window.location.pathname + window.location.search);
+}
+
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter onUpdate={logPageView}>
     <App />
   </BrowserRouter>
   , document.getElementById('root'));
