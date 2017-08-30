f4b5c3c9e7bf0abe834270d90b814707d9715b2e
Chang Rajani <chang.rajani@gmail.com>
2017-08-30 15:45:23 +0300

    Add queryform to FAQ

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -1,16 +1,5 @@
 import React, { Component } from 'react';
-import '../css/Search.css';
-import ChartSideBar from './ChartSideBar';
-import DosageChart from './DosageChart';
-import SearchBox from './SearchBox';
-import { getByKeyword } from '../util';
-import BasketModal from './BasketModal';
-import QuoteModal from './QuoteModal';
-import AssociatedChart from './charts/AssociatedChart';
-import Spinner from 'react-spinkit'
-import WarningText from './WarningText';
-import queryString from 'query-string';
-import postsSmallIcon from '../css/posts-small.svg';
+import QueryForm from './QueryForm';
 
 export default class FAQ extends Component {
 
@@ -26,6 +15,9 @@ export default class FAQ extends Component {
     return (
 
       <div className="faq-content">
+        <QueryForm
+          history={this.props.history}
+        />
         <div className="heading-3 white centered">
           FAQs
           <br/>
